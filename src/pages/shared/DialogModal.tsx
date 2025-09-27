75ca5043efdced7370da77a2ac668458ec3b0243
Suryadeep Bhujel <srdpbhujel@gmail.com>
2025-09-27 22:48:44 +0530

    refactor: integrate FormWidget for form handling in DialogModal

--- src/pages/shared/DialogModal.tsx
+++ src/pages/shared/DialogModal.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from "react";
-
+import FormWidget from "@src/pages/shared/FormWidget";
 type Props = {
     isDialogOpen: boolean;
     onClose: () => void;
@@ -8,8 +8,9 @@ type Props = {
     dialogType?: string;
     dialogTitle?: string;
     formStructure?: any;
-    formInputs?: any;
+    formModal?: any;
     formErrors?: any;
+    rows?: number;
 };
 const DialogModal: React.FC<Props> = ({
     isDialogOpen,
@@ -18,17 +19,18 @@ const DialogModal: React.FC<Props> = ({
     dialogType,
     dialogTitle,
     formStructure,
-    formInputs,
-    formErrors
+    formModal,
+    formErrors,
+    rows = 1
 }) => {
     console.log("isDialogOpenisDialogOpenisDialogOpen--->", isDialogOpen)
     const [localForm, setLocalForm] = useState<Record<string, any>>({});
     useEffect(() => {
         // create a shallow clone so edits in modal don't mutate parent's object
-        setLocalForm(formInputs ? { ...formInputs } : {});
-    }, [formInputs]);
+        setLocalForm(formModal ? { ...formModal } : {});
+    }, [formModal]);
     const submitFormData = () => {
-        console.log("formInputs--->", localForm)
+        console.log("formModal--->", localForm)
         onSave(localForm);
     }
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -65,12 +67,13 @@ const DialogModal: React.FC<Props> = ({
                                     </div>
                                 )}
                                 <form className="space-y-4" action="#" method="POST" onSubmit={(e) => { e.preventDefault(); submitFormData() }}>
-                                    {formStructure.map((field) => (
-                                        <div key={field.fieldName}>
-                                            <label htmlFor={field.fieldName} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{field.label}</label>
-                                            <input type={field.dataType} name={field.fieldName} onChange={handleInputChange} id={field.fieldName} value={localForm[field.fieldName] ?? ""} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder={`Enter your ${field.label.toLowerCase()}`} />
-                                        </div>
-                                    ))}
+                                    <FormWidget
+                                        formModel={localForm}
+                                        formStructure={formStructure}
+                                        onInputChange={handleInputChange}
+                                        formErrors={formErrors}
+                                        rows={rows}
+                                    />
                                     <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
                                         <button data-modal-hide="default-modal" type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save</button>
                                         <button data-modal-hide="default-modal" type="button" className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={onClose}>Cancel</button>
