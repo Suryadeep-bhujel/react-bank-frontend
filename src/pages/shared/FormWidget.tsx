1543e695798a04b993effc99fd4800d023123f70
Suryadeep Bhujel <srdpbhujel@gmail.com>
2025-09-27 15:34:20 +0530

    Common Form widget added

---
+++ src/pages/shared/FormWidget.tsx
@@ -0,0 +1,141 @@
+import type { FormStructure } from "@src/shared/SharedInterface";
+import { useEffect, useState } from "react";
+import Select from 'react-select'
+import { DateAndTimeService } from "../../../@bank-app-common/service/date-service";
+
+type Props = {
+    // onClose?: () => void;
+    // onSave: ({ }) => void;
+    onInputChange: ({ }) => void;
+    dialogTitle?: string;
+    formStructure: FormStructure[];
+    // formInputs?: any;
+    formErrors?: any;
+    rows?: number,
+    formModel: any,
+};
+const FormWidget: React.FC<Props> = ({
+    onInputChange,
+    formStructure,
+    // formInputs,
+    formModel,
+    formErrors,
+    rows = 4
+}) => {
+    return (
+        <>
+            {/* {`grid-cols-${rows}`} */}
+            <div>
+                {/* <form className="space-y-4" action="#" method="POST" onSubmit={(e) => { e.preventDefault() }}> */}
+                {formErrors && (
+                    <>
+                        {Array.isArray(formErrors) && Object.keys(formErrors).length > 0 && (
+                            <div className="mb-4">
+                                <ul className="list-disc list-inside text-sm text-red-600">
+                                    {Object.entries(formErrors).map(([field, errorMsg]) => (
+                                        <li key={field}>{errorMsg}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
+                        {!Array.isArray(formErrors) && typeof formErrors === 'string' && (
+                            <ul className="list-disc list-inside text-sm text-red-600">
+                                <li>{formErrors}</li>
+                            </ul>
+                        )}
+                    </>
+                )}
+                <div className="grid">
+                    {formStructure && formStructure.filter(formItem => formItem.visible && formItem.single).map((field) => (
+                        <div key={field.fieldName} >
+                            <label htmlFor={field.fieldName} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{field.label}</label>
+                            {field.dataType === 'options' && (
+                                <>
+                                    <Select
+                                        options={field?.options}
+                                        onChange={(selectedOption) => onInputChange({ target: { name: field.fieldName, value: selectedOption?.value } })}
+                                        name={field.fieldName}
+                                        value={field?.options?.find(option => option.value === formModel?.[field.fieldName]) ?? ""}
+                                        onKeyDown={(e) => {
+                                            const nextValue =
+                                                e.key.length === 1 // if it's a normal character
+                                                    ? e.target.value + e.key
+                                                    : e.key === "Backspace"
+                                                        ? e.target.value.slice(0, -1)
+                                                        : e.target.value;
+
+                                            field.searchItems ? field.searchItems(nextValue) : undefined;
+                                        }}
+                                    />
+                                </>
+                            )}
+                            {field.dataType !== 'options' && (
+                                <>
+                                    <input disabled= {field?.disabled} key={field.fieldName} type={field.dataType} name={field.fieldName} onChange={(e) => {
+                                        onInputChange(e);
+                                        field?.onChange(field.dataType === 'checkbox' ? e?.target?.checked : e?.target.value);
+                                    }} id={field.fieldName} value={formModel?.[field.fieldName] ?? ""} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder={`Enter your ${field.label}`} />
+                                </>
+                            )}
+                        </div>
+                    ))}
+                </div>
+
+                <div className={`grid grid-cols-${rows} gap-4`} style={{ gridTemplateColumns: `repeat(${rows}, minmax(0, 1fr))` }}>
+                    {formStructure && formStructure.filter(formItem => formItem.visible && !formItem.single).map((field) => (
+                        <div key={field.fieldName} >
+                            <label htmlFor={field.fieldName} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{field.label}</label>
+                            {field.dataType === 'options' && (
+                                <>
+                                    <Select
+                                        options={field?.options}
+                                        onChange={(selectedOption) => onInputChange({ target: { name: field.fieldName, value: selectedOption?.value } })}
+                                        name={field.fieldName}
+                                        value={field?.options?.find(option => option.value === formModel?.[field.fieldName]) ?? ""}
+                                        onKeyDown={(e) => {
+                                            const nextValue =
+                                                e.key.length === 1 // if it's a normal character
+                                                    ? e.target.value + e.key
+                                                    : e.key === "Backspace"
+                                                        ? e.target.value.slice(0, -1)
+                                                        : e.target.value;
+
+                                            field.searchItems ? field.searchItems(nextValue) : undefined;
+                                        }}
+                                    />
+                                    {/* <select
+                                        name={field.fieldName}
+                                        value={formModel?.[field.fieldName] ?? ""}
+                                        onChange={(e) => onInputChange({ fieldName: field.fieldName, value: e.target.value })}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    >
+                                        <option value="">Select {field?.label} </option>
+                                        {field?.options?.map((option) => (
+                                            <option key={option?.value} value={option?.value}>
+                                                {option?.label}
+                                            </option>
+                                        ))}
+                                    </select> */}
+                                </>
+                            )}
+                            {field.dataType !== 'options' && (
+                                <>
+                                    <input key={field.fieldName} type={field.dataType} name={field.fieldName} onChange={(e)=> {
+                                        if(field.dataType==='date'){
+                                            // e.target.value === DateAndTimeService.convertDate(formModel?.[field.fieldName])
+                                        }
+                                        onInputChange(e)
+                                    }} id={field.fieldName} value={formModel?.[field.fieldName] ?? ""} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder={`Enter your ${field.label}`} />
+                                </>
+                            )}
+                        </div>
+                    ))}
+
+                </div>
+
+                {/* </form> */}
+            </div>
+        </>
+    );
+};
+export default FormWidget;
